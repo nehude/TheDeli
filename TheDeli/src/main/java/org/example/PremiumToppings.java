0eb00f61ea906029e3aac5a5d8aad605e72c3b20
Tatieonna <tatieonnadestiny1999@icloud.com>
2023-11-27 11:14:49 -0600

    Array lists for meat, cheese, sauces, and toppings

--- TheDeli/src/main/java/org/example/PremiumToppings.java
+++ TheDeli/src/main/java/org/example/PremiumToppings.java
@@ -1,43 +1,88 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class PremiumToppings {
-    public String meatType;
-    public String cheeseType;
 
-    public PremiumToppings(String meatType, String cheeseType) {
-        this.meatType = meatType;
-        this.cheeseType = cheeseType;
-    }
+        private String meatType;
+        private String cheeseType;
 
-    public String getMeatType() {
-        return meatType;
-    }
+        private ArrayList<String> meatOptions;
+        private ArrayList<String> cheeseOptions;
 
-    public void setMeatType(String meatType) {
-        this.meatType = meatType;
-    }
+        public PremiumToppings(String meatType, String cheeseType) {
+            this.meatType = meatType;
+            this.cheeseType = cheeseType;
 
-    public String getCheeseType() {
-        return cheeseType;
-    }
+            meatOptions = new ArrayList<>();
+            meatOptions.add("Steak");
+            meatOptions.add("Ham");
+            meatOptions.add("Salami");
+            meatOptions.add("Roast Beef");
+            meatOptions.add("Chicken");
+            meatOptions.add("Bacon");
 
-    public void setCheeseType(String cheeseType) {
-        this.cheeseType = cheeseType;
-    }
-    public double addExtraMeat(){
-        if (meatType.equalsIgnoreCase("Angus Beef")){
-            return 1.50;
-        } else{
-            return 0.00;
+            cheeseOptions = new ArrayList<>();
+            cheeseOptions.add("American");
+            cheeseOptions.add("Provolone");
+            cheeseOptions.add("Cheddar");
+            cheeseOptions.add("Swiss");
+        }
+
+        public String getMeatType() {
+            return meatType;
+        }
+
+        public void setMeatType(String meatType) {
+            this.meatType = meatType;
+        }
+
+        public String getCheeseType() {
+            return cheeseType;
+        }
+
+        public void setCheeseType(String cheeseType) {
+            this.cheeseType = cheeseType;
+        }
+
+        public double addExtraMeat() {
+            if (meatType.equals("Steak")) {
+                return 3.50;
+            } else if (meatType.equals("Ham")) {
+                return 1.25;
+            } else if (meatType.equals("Salami")) {
+                return 1.50;
+            } else if (meatType.equals("Roast Beef")) {
+                return 2.00;
+            } else if (meatType.equals("Chicken")) {
+                return 2.00;
+            } else if (meatType.equals("Bacon")) {
+                return 1.50;
+            } else {
+                return 0.00;
+            }
+        }
+
+        public double addExtraCheese() {
+            if (cheeseType.equals("American")) {
+                return 0.50;
+            } else if (cheeseType.equals("Provolone")) {
+                return 1.00;
+            } else if (cheeseType.equals("Cheddar")) {
+                return 1.00;
+            } else if (cheeseType.equals("Swiss")) {
+                return 0.75;
+            } else {
+                return 0.00;
+            }
+        }
+
+        public ArrayList<String> getMeatOptions() {
+            return meatOptions;
+        }
+
+        public ArrayList<String> getCheeseOptions() {
+            return cheeseOptions;
         }
     }
-    public double addExtraCheese(){
-        if (cheeseType.equalsIgnoreCase("Pepper Jack ")){
-            return 0.75;
-        }else if(cheeseType.equalsIgnoreCase("Provolone")){
-            return 1.00;
-        }else{
-            return 0.00;
-        }
-    }
-}
+
--- TheDeli/src/main/java/org/example/RegularToppings.java
+++ TheDeli/src/main/java/org/example/RegularToppings.java
@@ -1,37 +1,70 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class RegularToppings {
-        private String toppingType;
-        private String toppingSauceType;
-        private String sideSauceType;
+    private String toppingType;
+    private String toppingSauceType;
+    private String sideSauceType;
+    private ArrayList<String> regularToppingsList;
+    private ArrayList<String> saucesList;
 
-        public RegularToppings(String toppingType, String toppingSauceType, String sideSauceType) {
-            this.toppingType = toppingType;
-            this.toppingSauceType = toppingSauceType;
-            this.sideSauceType = sideSauceType;
-        }
 
-        public String getToppingType() {
-            return toppingType;
-        }
+    public RegularToppings(String toppingType, String toppingSauceType, String sideSauceType) {
+        this.toppingType = toppingType;
+        this.toppingSauceType = toppingSauceType;
+        this.sideSauceType = sideSauceType;
 
-        public void setToppingType(String toppingType) {
-            this.toppingType = toppingType;
-        }
+        regularToppingsList = new ArrayList<>();
+        regularToppingsList.add("lettuce");
+        regularToppingsList.add("peppers");
+        regularToppingsList.add("onions");
+        regularToppingsList.add("tomatoes");
+        regularToppingsList.add("jalapenos");
+        regularToppingsList.add("cucumbers");
+        regularToppingsList.add("pickles");
+        regularToppingsList.add("guacamole");
+        regularToppingsList.add("mushrooms");
 
-        public String getToppingSauceType() {
-            return toppingSauceType;
-        }
-
-        public void setToppingSauceType(String toppingSauceType) {
-            this.toppingSauceType = toppingSauceType;
-        }
-
-        public String getSideSauceType() {
-            return sideSauceType;
-        }
-
-        public void setSideSauceType(String sideSauceType) {
-            this.sideSauceType = sideSauceType;
-        }
+        saucesList = new ArrayList<>();
+        saucesList.add("mayo");
+        saucesList.add("mustard");
+        saucesList.add("ketchup");
+        saucesList.add("ranch");
+        saucesList.add("thousand island");
+        saucesList.add("vinaigrette");
     }
+
+    public String getToppingType() {
+        return toppingType;
+    }
+
+    public void setToppingType(String toppingType) {
+        this.toppingType = toppingType;
+    }
+
+    public String getToppingSauceType() {
+        return toppingSauceType;
+    }
+
+    public void setToppingSauceType(String toppingSauceType) {
+        this.toppingSauceType = toppingSauceType;
+    }
+
+    public String getSideSauceType() {
+        return sideSauceType;
+    }
+
+    public void setSideSauceType(String sideSauceType) {
+        this.sideSauceType = sideSauceType;
+
+    }
+
+    public ArrayList<String> getRegularToppingsList() {
+        return regularToppingsList;
+    }
+
+    public ArrayList<String> getSaucesList() {
+        return saucesList;
+    }
+}
